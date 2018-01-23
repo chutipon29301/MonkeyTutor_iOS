ce551063b330ac2f7db730becbe18ff4eb1c0ed3
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2018-01-23 14:01:17 +0700

    Add logout method

--- MonkeyTutor/Controllers/SettingTableViewController.swift
+++ MonkeyTutor/Controllers/SettingTableViewController.swift
@@ -7,10 +7,51 @@
 //
 
 import UIKit
+import BulletinBoard
 
 class SettingTableViewController: UITableViewController {
     
+    lazy var bulletinManager: BulletinManager = {
+        let page = PageBulletinItem(title: "Logout")
+        page.descriptionText = "Are you sure you want to logout"
+        page.actionButtonTitle = "Logout"
+        page.alternativeButtonTitle = "Not now"
+        page.actionHandler = { (item: PageBulletinItem) in
+            self.parent?.parent?.dismiss(animated: true, completion: nil)
+        }
+        page.alternativeHandler = { (item: PageBulletinItem) in
+            self.dismissBulletin()
+        }
+        page.isDismissable = true
+        let rootItem: BulletinItem = page
+        return BulletinManager(rootItem: rootItem)
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        switch indexPath.section {
+        case 0:
+            break
+        case 1:
+            switch indexPath.row{
+            case 0:
+                tableView.deselectRow(at: indexPath, animated: true)
+                bulletinManager.prepare()
+                bulletinManager.presentBulletin(above: self)
+                break
+            default:
+                break
+            }
+            break
+        default:
+            break
+        }
+    }
+    
+    func dismissBulletin() {
+        bulletinManager.dismissBulletin()
     }
 }
